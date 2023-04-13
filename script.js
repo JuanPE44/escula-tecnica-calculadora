3f4dd3822e2ecd6eef3bc8c0e1c5d94eb08dcd94
Benjagodeto1 <alexisespind@gmail.com>
2023-04-12 23:52:57 -0300

    styles

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 const botones = document.querySelectorAll("button")
 const input = document.querySelector("input")
 let resultado 
+let Suma = false 
+let Resta = false
+let Multiplicacion = false
+let Division = false
+
 
 botones.forEach(function(boton){
     boton.addEventListener("click", mostrar)
-
 })
 
 function mostrar(e){
-
-   if(e.target.innerHTML== '='){
-    crearesultado() 
-   }
-   else if(e.target.innerHTML=='AC'){
-   input.value = ""
-   }
-
-   else { 
-    input.value += e.target.innerHTML
-   }
-  
-}
-
-function crearesultado(){
+    
+    const teclaPresionada = e.target.innerHTML
+    if (teclaPresionada === '=') {
+       crearesultado()
+    } else if (teclaPresionada === 'AC') {
+       input.value = ""
+    } else {
+       if (teclaPresionada === '+' && Suma) {
+          return 
+       } else if (teclaPresionada === '-' && Resta) {
+          return 
+       } else if (teclaPresionada === '*' && Multiplicacion) {
+          return 
+       } else if (teclaPresionada === '/' && Division) {
+          return 
+       }
+ 
+       input.value += teclaPresionada
+       
+       if (teclaPresionada === '+') {
+          Suma = true
+       } else if (teclaPresionada === '-') {
+          Resta = true
+       } else if (teclaPresionada === '*') {
+          Multiplicacion = true
+       } else if (teclaPresionada === '/') {
+          Division = true
+       }
+    }
+ }
+   
+ function crearesultado(){
     resultado = eval(input.value)
     console.log(resultado)
     input.value = resultado
+    falso()
+}
+
+function falso(){
+    Suma = false;
+    Resta = false
+    Multiplicacion = false
+    Division = false
+    return
 }
 
